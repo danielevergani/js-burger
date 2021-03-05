15c209f7b21fd16835baeb747e5e96aa9735f01e
danielevergani <dvergani94@gmail.com>
2021-03-05 15:50:13 +0100

    inseriti value della checkbox

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+// sezione di inizializzazione variabili
 var burgerName = document.getElementById("burger__name");
 var calculate = document.getElementById("calculate");
-console.log(burgerName.value);
-console.log(calculate.value);
+
+// inserisco evento click
+
+calculate.addEventListener("click",
+
+    function() {
+
+        // condizioni al click
+
+        if ( burgerName.value != "" ){
+            
+        }
+        else{
+            alert("scrivi il nume del burger")
+        }
+    }
+);
