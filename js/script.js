03f3f0978de80e445809d0cc22085300be15d4e7
danielevergani <dvergani94@gmail.com>
2021-03-05 16:02:17 +0100

    inserito if x verificare se checked true

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // sezione di inizializzazione variabili
 var burgerName = document.getElementById("burger__name");
 var calculate = document.getElementById("calculate");
-
+var selection = document.getElementsByClassName("selection");
+console.log(selection);
 // inserisco evento click
 
 calculate.addEventListener("click",
@@ -11,7 +12,15 @@ calculate.addEventListener("click",
         // condizioni al click
 
         if ( burgerName.value != "" ){
-            
+
+            // navigo la lista di checkbox
+            for ( var i = 0; i < selection.length; i++ ){
+
+                // verifico condizione con checked
+                if (selection[i].checked){
+                    console.log("ok");
+                }
+            }
         }
         else{
             alert("scrivi il nume del burger")
